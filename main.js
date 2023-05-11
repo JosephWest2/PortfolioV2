fd52cea1bbd0f78ab326512ca4716b7a81d134ad
Jwest11 <joeywest1414@gmail.com>
2023-05-11 15:58:53 -0600

    further updates

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ function changeActiveColor(color) {
 
     document.documentElement.style.setProperty("--color-active", colors[color]);
 }
+
+let focusStatus = false;
+
+function focusProject(id) {
+    if (focusStatus === true) {focusStatus = false; return;}
+    const project = document.getElementById(id);
+    project.dataset.focused = "true";
+    focusStatus = true;
+}
+
+function deFocusProject(id) {
+    const project = document.getElementById(id);
+    project.dataset.focused = "leaving";
+    setTimeout(() => {
+        project.dataset.focused = "false"
+    }, 300)
+}
