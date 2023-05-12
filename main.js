ce53c493a456a530311917dd36f5815abd608004
Jwest11 <joeywest1414@gmail.com>
2023-05-11 23:59:04 -0600

    updatesss

--- main.js
+++ main.js
@@ -25,3 +25,37 @@ function deFocusProject(id) {
         project.dataset.focused = "false"
     }, 300)
 }
+
+function deFocusAllProjects() {
+    const projects = document.querySelectorAll(".project");
+    projects.forEach(project => {
+        if (project.dataset.focused == "true") {
+            deFocusProject(project.id);
+        };
+    })
+}
+
+const growingContent = document.getElementById("growing-content");
+growingContent.addEventListener("click", (e) => {
+    if (e.target === growingContent) {deFocusAllProjects(); focusStatus = false;};
+})
+
+
+const skillsContainer = document.getElementById("skillsContainer");
+const skills = document.querySelectorAll(".skill");
+const skillsCount = skills.length;
+const skillsContainerRect = skillsContainer.getBoundingClientRect();
+let radius = Math.min(skillsContainerRect.width/2, skillsContainerRect.height/2);
+let center = (skillsContainerRect.width/2, skillsContainerRect.height/2);
+const PI = 3.14159;
+
+let circleRotation = 0;
+let deltaRadians = PI * 2 /skills.length;
+let iterationRadians = 0;
+
+
+skills.forEach(skillNode => {
+    let nodeX = interationRadians;
+})
+
+
